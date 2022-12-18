abfbec5cce5997f9be3ded976668867409840506
Kinni Yya Lopez <kinniyya19@gmail.com>
2022-12-18 15:08:48 +0800

    Added an archive button

--- class/with-class.php
+++ class/with-class.php
@@ -19,18 +19,20 @@ if (checkClassJoin($_SESSION['user_id']) == FALSE) {
     <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="../css/sections.css">
     <link rel="stylesheet" href="../css/with-class.css">
+    <script src="https://kit.fontawesome.com/c11ce9a287.js" crossorigin="anonymous"></script>
 </head>
-<body id="scrll">
-        <?php DisplayNavHeader(); ?>
 
-        <div class="container">
-            <div class="website-content">
-                <?php
-                // GETs the parameter from the URL php?parameter=[value]&anohterParamaterifthereis=[value]
-                // echo "<h1>" . SelectClassName($_GET['class_id']) . "</h1>";
-                echo "<p>Welcome " . SelectUserName($_SESSION['user_id']) . "!<p>";
-                ?>
-            </div>
+<body id="scrll">
+    <?php DisplayNavHeader(); ?>
+
+    <div class="container">
+        <div class="website-content">
+            <?php
+            // GETs the parameter from the URL php?parameter=[value]&anohterParamaterifthereis=[value]
+            // echo "<h1>" . SelectClassName($_GET['class_id']) . "</h1>";
+            echo "<p>Welcome " . SelectUserName($_SESSION['user_id']) . "!<p>";
+            ?>
+        </div>
 
         <div class="cardwclass">
             <div class="buttons">
@@ -43,43 +45,44 @@ if (checkClassJoin($_SESSION['user_id']) == FALSE) {
 
     <!-- SHOW CLASSES OF THE USER -->
     <!-- <div id="blur2"> -->
-        <div class="case">
-            <?php
-            $class_info = GetClass($_SESSION['user_id']);
-            /**
-             * 1. WHAT IS "$class_info->fetch_assoc())"
-             *       = convert class_info (aka results GetClass()) into a dictionary/array
-             * 2. LOOP TILL END OF DATA (kailangan si $rows kasi doon naka store yung naconvert)
-             */
-            while ($rows = $class_info->fetch_assoc()) {    ?>
-                <div class="card">
-                    <div class="cname">
-                        <p><?php echo $rows['class_name']; ?></p>
-                    </div>
-                    <p>Class Code: <span><?php echo $rows['class_code']; ?></span></p></br>
-                    <p>SY: <?php
-                            echo date("Y", strtotime($rows['school_year']));
-                            echo "-";
-                            echo date("Y", strtotime($rows['school_year'])) + 1; ?></p>
-                    <form method="GET" action="note.php">
-                        <input type="hidden" name="class_id" value="<?php echo $rows['class_id']; ?>">
-                    </form>
-                    </br>
-                    <div class="buttonswclass">
-                        <button type="button" class="view" onclick="location.href='../notes/note.php?class_id=<?php echo $rows['class_id']; ?>&tab=due'">VIEW CLASS</button>
-
-                        <?php
-                        // Check if the creator id of the class is the user logged in
-                        $manage_class_id = checkManageClass($rows['class_id'], $_SESSION['user_id']);
-                        if ($manage_class_id == TRUE) {
-                            echo "<button type=\"button\" class=\"view\" onclick=\"location.href='manage-class.php?class_id=" . $rows['class_id'] . "'\">MANAGE CLASS</button></br></br>";
-                        } ?>
-                    </div>
+    <div class="case">
+        <?php
+        $class_info = GetClass($_SESSION['user_id']);
+        /**
+         * 1. WHAT IS "$class_info->fetch_assoc())"
+         *       = convert class_info (aka results GetClass()) into a dictionary/array
+         * 2. LOOP TILL END OF DATA (kailangan si $rows kasi doon naka store yung naconvert)
+         */
+        while ($rows = $class_info->fetch_assoc()) {    ?>
+            <div class="card">
+            <i class="fa-solid fa-box-archive" onclick="location.href='#'"></i>
+                <div class="cname">
+                    <p><?php echo $rows['class_name']; ?></p>
                 </div>
-            <?php
-            }
-            ?>
-        </div>
+                <p>Class Code: <span><?php echo $rows['class_code']; ?></span></p></br>
+                <p>SY: <?php
+                        echo date("Y", strtotime($rows['school_year']));
+                        echo "-";
+                        echo date("Y", strtotime($rows['school_year'])) + 1; ?></p>
+                <form method="GET" action="note.php">
+                    <input type="hidden" name="class_id" value="<?php echo $rows['class_id']; ?>">
+                </form>
+                </br>
+                <div class="buttonswclass">
+                    <button type="button" class="view" onclick="location.href='../notes/note.php?class_id=<?php echo $rows['class_id']; ?>&tab=due'">VIEW CLASS</button>
+
+                    <?php
+                    // Check if the creator id of the class is the user logged in
+                    $manage_class_id = checkManageClass($rows['class_id'], $_SESSION['user_id']);
+                    if ($manage_class_id == TRUE) {
+                        echo "<button type=\"button\" class=\"view\" onclick=\"location.href='manage-class.php?class_id=" . $rows['class_id'] . "'\">MANAGE CLASS</button></br></br>";
+                    } ?>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
     <!-- </div> -->
     <div id="blur"></div>
     <!-- POPUP -->
@@ -104,47 +107,45 @@ if (checkClassJoin($_SESSION['user_id']) == FALSE) {
     <!-- AJAX / jQuery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-<!-- Get the form value and send to the with-class-process for joining class -->
-<script type="text/javascript">
-$("#formJoinClass").submit(function(e){
-    e.preventDefault();
-    var form = $(this);
+    <!-- Get the form value and send to the with-class-process for joining class -->
+    <script type="text/javascript">
+        $("#formJoinClass").submit(function(e) {
+            e.preventDefault();
+            var form = $(this);
 
-    $.ajax({
-        type: 'POST',
-        url: 'with-class-process.php',
-        data: form.serialize(),
-        success: function(data){
-            if(data > 0){
-                alert("Class joined successfully!");
-                // window.location.replace("../notes/note.php?class_id=" + data);
-                window.location.replace("with-class.php");
-            }
-            else if(data == 0){
-                alert("Class doesn't exist!");
-            }
-            else{
-                alert(data);
-            }
+            $.ajax({
+                type: 'POST',
+                url: 'with-class-process.php',
+                data: form.serialize(),
+                success: function(data) {
+                    if (data > 0) {
+                        alert("Class joined successfully!");
+                        // window.location.replace("../notes/note.php?class_id=" + data);
+                        window.location.replace("with-class.php");
+                    } else if (data == 0) {
+                        alert("Class doesn't exist!");
+                    } else {
+                        alert(data);
+                    }
+                }
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        function openAddClassForm() {
+            document.getElementById("add-class-form").style.display = "block";
+            document.getElementById('blur').style.filter = "blur(5px)";
+            document.getElementById('blur').style.display = "block";
+            document.getElementById('scrll').style.overflow = "hidden";
         }
-    });
-});
-</script>
-<script type="text/javascript">
-            function openAddClassForm() {
-                document.getElementById("add-class-form").style.display = "block";
-                document.getElementById('blur').style.filter = "blur(5px)";
-                document.getElementById('blur').style.display = "block";
-                document.getElementById('scrll').style.overflow = "hidden";
-            }
 
-            function closeAddClassForm() {
-                document.getElementById("add-class-form").style.display = "none";
-                document.getElementById('blur').style.filter = "blur(0)";
-                document.getElementById('blur').style.display = "none";
-                document.getElementById('scrll').style.overflow = "auto";
-            }
-</script>
+        function closeAddClassForm() {
+            document.getElementById("add-class-form").style.display = "none";
+            document.getElementById('blur').style.filter = "blur(0)";
+            document.getElementById('blur').style.display = "none";
+            document.getElementById('scrll').style.overflow = "auto";
+        }
+    </script>
 
     <!-- 
     VIEW CLASSES
@@ -163,4 +164,5 @@ $("#formJoinClass").submit(function(e){
 -->
 
 </body>
+
 </html>
