ed20bb8c123f76d2086aa55fd7a202d2bc20d960
Kinni Yya Lopez <kinniyya19@gmail.com>
2022-12-20 01:06:33 +0800

    Added select and update user note record

--- sql/my-list-sql.php
+++ sql/my-list-sql.php
@@ -22,7 +22,37 @@ function InsertUserNote($user_id, $due_date, $due_time, $note_title, $descriptio
         ($due_time == null ? "NULL" : "'$due_time'")
         . ", '$note_title', '$description')";
     if ($conn->multi_query($sql) === TRUE) {
-        echo "Note added successfully" . $sql;
+        echo "Note added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+
+function SelectUserNoteRecord($note_id)
+{
+    $conn = OpenCon();
+    $sql = "SELECT note_id, post_date, due_date, due_time, note_title, description
+			FROM user_note
+            WHERE note_id = '$note_id'";
+    $result = $conn->query($sql);
+    $conn->close();
+    return $result->fetch_assoc();
+}
+
+function UpdateUserNote($note_id, $user_id, $due_date, $due_time, $note_title, $description, $conn)
+{
+    $offset = SynchTimeZone();
+    $sql = "SET time_zone='$offset';
+
+			UPDATE user_note
+			SET user_id = " . ($user_id == null ? "NULL" : "'$user_id'") . ", 
+                due_date = " . ($due_date == null ? "NULL" : "'$due_date'") . ", 
+                due_time = " . ($due_time == null ? "NULL" : "'$due_time'") . ", 
+                note_title = '$note_title', description = '$description'
+			WHERE note_id = '$note_id'";
+    if ($conn->multi_query($sql) === TRUE) {
+        echo "Note updated successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
