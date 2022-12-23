da21c330dccc28132b56d3d56cb7a4802a5a13ee
jimlozada05 <109643475+jimlozada05@users.noreply.github.com>
2022-12-22 22:37:49 -0800

    Add files via upload
    Calendar function changes

--- sql/calendar-sql.php
+++ sql/calendar-sql.php
@@ -14,12 +14,14 @@ function SelectClassCalendar($class_id, $member_id){
     return $result;
 }
 
-// Insert into calendar table
+// Insert into calendar
 function InsertClassCalendarEvent($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $conn){
 	$sql = "INSERT INTO calendar(event_title, description, start_datetime, end_datetime, class_id, subject_id)
 			VALUES ('$event_title', ".($description == null ? "NULL" : "'$description'").", '$start_datetime', '$end_datetime', '$class_id', ".($subject_id == null ? "NULL" : "'$subject_id'").")";
 	if ($conn->query($sql) === TRUE) {
-	  echo "Event created!";
+		// Create note after the calendar
+		InsertNoteClassCalendar($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $conn->insert_id);
+	  	echo "Event created!";
 	} else {
 	  echo "Error: " . $sql . "<br>" . $conn->error;
 	}
@@ -32,7 +34,9 @@ function UpdateClassCalendarEvent($event_id, $event_title, $description, $start_
 			SET event_title = '$event_title', description = ".($description == null ? "NULL" : "'$description'").", start_datetime = '$start_datetime', end_datetime = '$end_datetime', class_id = '$class_id', subject_id = ".($subject_id == null ? "NULL" : "'$subject_id'")."
 			WHERE event_id = '$event_id'";
 	if ($conn->query($sql) === TRUE) {
-	  echo "Event modified!";
+		// Update the note after the calendar
+		UpdateNoteClassCalendar($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $event_id);
+	  	echo "Event modified!";
 	} else {
 	  echo "Error: " . $sql . "<br>" . $conn->error;
 	}
@@ -44,13 +48,16 @@ function DeleteClassCalendarEvent($event_id, $conn){
 	$sql = "DELETE FROM calendar
 			WHERE event_id = '$event_id'";
 	if ($conn->query($sql) === TRUE) {
-	  echo "Event removed!";
+		// Remove the note after removing the calendar
+		DeleteNoteClassCalendar($event_id);
+	  	echo "Event removed!";
 	} else {
 	  echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 	$conn->close();
 }
 
+// Select the calendar per user
 function SelectCalendarRecord($user_id){
 	$conn = OpenCon();
 	// Unioned the table that the user can see after excluding the unenroll and the table for user calendar
@@ -65,7 +72,10 @@ function SelectCalendarRecord($user_id){
 				WHERE user_id = '$user_id' AND (subject_id NOT IN (
 					SELECT subject_id
 					FROM unenroll
-					WHERE member_id = member.member_id) OR subject_id IS NULL))
+					WHERE member_id = member.member_id) OR subject_id IS NULL) AND calendar.class_id NOT IN(
+					SELECT class_id
+					FROM archive_class
+					WHERE user_id = '$user_id'))
 				UNION ALL
 				(SELECT (@c_id := @c_id + 1) AS 'calendar_id', user_calendar.*, NULL as 'class_id'
 				FROM user_calendar
@@ -117,14 +127,62 @@ function DeleteUserCalendar($event_id, $conn){
 }
 
 // Insert into calendar table from insert note
-function InsertClassCalendarNote($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id){
+function InsertClassCalendarNote($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $note_id){
 	$conn = OpenCon();
+	// Insert into the calendar table
 	$sql = "INSERT INTO calendar(event_title, description, start_datetime, end_datetime, class_id, subject_id)
 			VALUES ('$event_title', ".($description == null ? "NULL" : "'$description'").", '$start_datetime', '$end_datetime', '$class_id', ".($subject_id == null ? "NULL" : "'$subject_id'").")";
 	if ($conn->query($sql) === TRUE) {
-	  echo "Event created!";
+		// Insert into the note_calendar table so the calendar and note can be referenced
+		$sql = "INSERT INTO note_calendar (note_id, event_id)
+				VALUES ('$note_id', '".$conn->insert_id."')";
+		if ($conn->query($sql) === TRUE) {
+			echo "Event created! \n";
+		} else {
+	 	 	echo "Error: " . $sql . "<br>" . $conn->error;
+	 	}
 	} else {
 	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
+}
+// Update the calendar table when the note is edited
+function UpdateClassCalendarNote($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $note_id){
+	$conn = OpenCon();
+	$sql = "SELECT *
+			FROM note_calendar
+			WHERE note_id = '$note_id'";
+	$result = $conn->query($sql);
+	// Check if there is a connection from calendar and note using the note_calendar
+	if($result->num_rows > 0){
+		// There is a calendar event for the note
+		$sql = "UPDATE calendar
+				SET event_title = '$event_title', description = '$description', start_datetime = '$start_datetime', end_datetime = '$end_datetime', class_id = '$class_id', subject_id = ".($subject_id == null ? "NULL" : "'$subject_id'")."
+				WHERE event_id = (SELECT event_id
+				FROM note_calendar
+				WHERE note_id = '$note_id')";
+		if ($conn->query($sql) === TRUE) {
+		  echo "Event updated! \n";
+		} else {
+		  echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+	else{
+		// There is no calendar event for the note
+		// Create calendar event
+		$sql = "INSERT INTO calendar(event_title, description, start_datetime, end_datetime, class_id, subject_id)
+				VALUES ('$event_title', ".($description == null ? "NULL" : "'$description'").", '$start_datetime', '$end_datetime', '$class_id', ".($subject_id == null ? "NULL" : "'$subject_id'").")";
+		// After creating the event create the note_calendar row to connect them for future tables
+		if ($conn->query($sql) === TRUE) {
+			// Insert into the note_calendar table so the calendar and note can be referenced
+			$sql = "INSERT INTO note_calendar (note_id, event_id)
+					VALUES ('$note_id', '".$conn->insert_id."')";
+			if ($conn->query($sql) === TRUE) {
+				echo "Event updated! \n";
+			} else {
+		 	 	echo "Error: " . $sql . "<br>" . $conn->error;
+		 	}
+		}
 	}
 	$conn->close();
 }
--- sql/notes-sql.php
+++ sql/notes-sql.php
@@ -1,48 +1,11 @@
 <?php
-// Check what member type is a user 0 for regular and 1 for officers
-function MemberInfo($user_id, $class_id){
-	$conn = OpenCon();
-	$sql = "SELECT member_id, member_type
-			FROM member
-			WHERE user_id = $user_id AND class_id = $class_id";
-	$result = $conn->query($sql);
-	$conn -> close();
-	return $result;
-}
-
-// Get the information about a specific class
-function GetClassRecord($class_id){
-	$conn = OpenCon();
-	$sql = "SELECT * 
-			FROM class 
-			WHERE class_id = $class_id";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc(); 
-	$conn -> close();
-	return $row;
-}
-
-// Get all the subject names from a given class
-function GetAMemberSubjectNames($member_id, $class_id){
-	$conn = OpenCon();
-	$sql = "SELECT subject_id, subject_name
-			FROM subject
-			WHERE class_id = '$class_id' AND subject_id NOT IN (
-				SELECT subject_id
-				FROM unenroll
-				WHERE member_id = '$member_id')";
-	$result = $conn->query($sql);
-	$conn -> close();
-	return $result;
-}
-
 // Insert to note table
 function InsertNote($class_id, $subject_id, $due_date, $due_time, $note_title, $description, $conn){
 	$offset = SynchTimeZone();
 	// Change the timezone first then proceed with the query, this is a multi_query instead of a query
-	$sql = "SET time_zone='$offset';
-
-			INSERT INTO note (class_id, subject_id, post_date, due_date, due_time, note_title, description)
+	$sql = "SET time_zone='$offset'";
+	$conn->query($sql);
+	$sql = "INSERT INTO note (class_id, subject_id, post_date, due_date, due_time, note_title, description)
 			VALUES ('$class_id', ".
 			($subject_id == null ? "NULL" : "'$subject_id'")
 			.", CURDATE(), ".
@@ -50,7 +13,60 @@ function InsertNote($class_id, $subject_id, $due_date, $due_time, $note_title, $
 			.", ".
 			($due_time == null ? "NULL" : "'$due_time'")
 			.", '$note_title', '$description')";
+	if ($conn->query($sql) === TRUE) {
+		// Check if there is a due date then add the note to calendar
+		if($due_date != null){
+			// If due time is null then set it as 08 AM
+			if($due_time == null){
+				$due_time = "08:00:00";
+			}
+			$start_datetime = date('Y-m-d H:i:s', strtotime("$due_date $due_time"));
+			$end_datetime = date('Y-m-d H:i:s', strtotime($start_datetime . "+30 minutes"));
+			// Insert into calendar table and pass the note_id as the last insert id
+			InsertClassCalendarNote($note_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $conn->insert_id);
+		}
+		echo "Note added successfully";
+	}
+	else{
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn -> close();
+}
+
+// Update note table
+function UpdateNote($note_id, $subject_id, $due_date, $due_time, $note_title, $description, $class_id, $conn){
+	$offset = SynchTimeZone();
+	// Change the timezone first then proceed with the query, this is a multi_query instead of a query
+	$sql = "SET time_zone='$offset';
+
+			UPDATE note
+			SET subject_id = ".($subject_id == null ? "NULL" : "'$subject_id'").", due_date = ".($due_date == null ? "NULL" : "'$due_date'").", due_time = ".($due_time == null ? "NULL" : "'$due_time'").", note_title = '$note_title', description = '$description'
+			WHERE note_id = '$note_id'";
 	if ($conn->multi_query($sql) === TRUE) {
+		// Check if there is a due date then add the note to calendar
+		if($due_date != null){
+			// If due time is null then set it as 08 AM
+			if($due_time == null){
+				$due_time = "08:00:00";
+			}
+			$start_datetime = date('Y-m-d H:i:s', strtotime("$due_date $due_time"));
+			$end_datetime = date('Y-m-d H:i:s', strtotime($start_datetime . "+30 minutes"));
+			// Update note table then update calendar table using the note id
+			UpdateClassCalendarNote($note_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $note_id);
+		}
+		if($due_date == null){
+			// if due date is null then the calendar event must be deleted
+			$conn->next_result();
+			// The next result command is needed since it is not possible to run two simultatnues query like DELETE, INSERT, or UPDATE
+			$sql = "DELETE FROM calendar
+					WHERE event_id = (SELECT event_id
+						FROM note_calendar
+						WHERE note_id = '$note_id');
+					DELETE FROM note_calendar
+					WHERE note_id = '$note_id';";
+			$conn->multi_query($sql);
+
+		}
 		echo "Note updated successfully";
 	}
 	else{
@@ -99,22 +115,65 @@ function SelectAnnouncementRecord($class_id, $member_id){
 	return $result;
 }
 
-// Update note table
-function UpdateNote($note_id, $subject_id, $due_date, $due_time, $note_title, $description, $conn){
-	$offset = SynchTimeZone();
-	// Change the timezone first then proceed with the query, this is a multi_query instead of a query
-	$sql = "SET time_zone='$offset';
-
-			UPDATE note
-			SET subject_id = ".($subject_id == null ? "NULL" : "'$subject_id'").", due_date = ".($due_date == null ? "NULL" : "'$due_date'").", due_time = ".($due_time == null ? "NULL" : "'$due_time'").", note_title = '$note_title', description = '$description'
-			WHERE note_id = '$note_id'";
-	if ($conn->multi_query($sql) === TRUE) {
-		echo "Note updated successfully";
-	}
-	else{
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
+// Check what member type is a user 0 for regular and 1 for officers
+function MemberInfo($user_id, $class_id){
+	$conn = OpenCon();
+	$sql = "SELECT member_id, member_type
+			FROM member
+			WHERE user_id = $user_id AND class_id = $class_id";
+	$result = $conn->query($sql);
 	$conn -> close();
+	return $result;
+}
+
+// Get all the subject names from a given class
+function GetAMemberSubjectNames($member_id, $class_id){
+	$conn = OpenCon();
+	$sql = "SELECT subject_id, subject_name
+			FROM subject
+			WHERE class_id = '$class_id' AND subject_id NOT IN (
+				SELECT subject_id
+				FROM unenroll
+				WHERE member_id = '$member_id')";
+	$result = $conn->query($sql);
+	$conn -> close();
+	return $result;
+}
+
+// Get the information about a specific class
+function GetClassRecord($class_id){
+	$conn = OpenCon();
+	$sql = "SELECT * 
+			FROM class 
+			WHERE class_id = $class_id";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc(); 
+	$conn -> close();
+	return $row;
+}
+
+// Get the subject name using the subject ID
+function SelectSubjectName($subject_id){
+	$conn = OpenCon();
+	$sql = "SELECT subject_name
+			FROM subject
+			WHERE subject_id = '$subject_id'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$conn -> close();
+	return $row['subject_name'];
+}
+
+// Select a single note record using note id
+function SelectANoteRecord($note_id){
+	$conn = OpenCon();
+	$sql = "SELECT *
+			FROM note
+			WHERE note_id = '$note_id'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$conn -> close();
+	return $row;
 }
 
 // Update note_status from the note_table
@@ -126,6 +185,18 @@ function ArchiveNote($note_id, $member_id, $conn){
 	}
 	else{
 		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn -> close();
+}
+
+// Delete from archive_note to restore the note
+function RestoreArchiveNote($archive_note_id, $conn){
+	$sql = "DELETE FROM archive_note
+			WHERE archive_note_id = '$archive_note_id'";
+	if ($conn->query($sql) === TRUE) {
+	  echo "Note restored successfully";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 	$conn -> close();
 }
@@ -148,18 +219,6 @@ function SelectArchiveNote($class_id, $member_id){
 	return $result;
 }
 
-// Delete from archive_note to restore the note
-function RestoreArchiveNote($archive_note_id, $conn){
-	$sql = "DELETE FROM archive_note
-			WHERE archive_note_id = '$archive_note_id'";
-	if ($conn->query($sql) === TRUE) {
-	  echo "Note restored successfully";
-	} else {
-	  echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-	$conn -> close();
-}
-
 // Add values to Pending_Note which is from the suggest note
 function AddNotePending($subject_id, $due_date, $due_time, $note_title, $description, $member_id, $class_id, $conn){
 	$offset = SynchTimeZone();
@@ -175,18 +234,6 @@ function AddNotePending($subject_id, $due_date, $due_time, $note_title, $descrip
 	echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 	$conn->close();
-}
-
-// Select all from the pending_note table filtered by member_id or who is logged in
-function SelectApprovalNote($class_id, $member_id){
-	$conn = OpenCon();
-	$sql = "SELECT *
-			FROM pending_note
-			WHERE class_id = '$class_id' AND member_id = '$member_id'
-			ORDER BY pending_note_id DESC";
-	$result = $conn->query($sql);
-	$conn -> close();
-	return $result;
 }
 
 // Delete a pending note
@@ -218,6 +265,18 @@ function AddCorrectionPending($note_id, $subject_id, $post_date, $due_date, $due
 	$conn->close();
 }
 
+// Select all from the pending_note table filtered by member_id or who is logged in
+function SelectApprovalNote($class_id, $member_id){
+	$conn = OpenCon();
+	$sql = "SELECT *
+			FROM pending_note
+			WHERE class_id = '$class_id' AND member_id = '$member_id'
+			ORDER BY pending_note_id DESC";
+	$result = $conn->query($sql);
+	$conn -> close();
+	return $result;
+}
+
 // Select pending note of the class
 function SelectPendingNote($class_id){
 	$conn = OpenCon();
@@ -227,30 +286,6 @@ function SelectPendingNote($class_id){
 	$result = $conn->query($sql);
 	$conn -> close();
 	return $result;
-}
-
-// Get the subject name using the subject ID
-function SelectSubjectName($subject_id){
-	$conn = OpenCon();
-	$sql = "SELECT subject_name
-			FROM subject
-			WHERE subject_id = '$subject_id'";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	$conn -> close();
-	return $row['subject_name'];
-}
-
-// Select a single note record using note id
-function SelectANoteRecord($note_id){
-	$conn = OpenCon();
-	$sql = "SELECT *
-			FROM note
-			WHERE note_id = '$note_id'";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	$conn -> close();
-	return $row;
 }
 
 //Process the pendinte_note if accepted change status to 2 if rejected change status to 1
@@ -273,7 +308,7 @@ function ProcessPendingNote($pending_note_id, $note_id, $status){
 		$offset = SynchTimeZone();
 		// Check if note_id is not Null, if it is the note has been suggested
 		if($note_id != NULL){
-		$sql = "SET time_zone='$offset';
+			$sql = "SET time_zone='$offset';
 
 				INSERT INTO note_history (note_id, pending_note_id, prev_subject_id, prev_due_date, prev_due_time, prev_note_title, prev_description, change_date)
 				SELECT note_id, '$pending_note_id', subject_id, due_date, due_time, note_title, description, CURDATE()
@@ -431,5 +466,65 @@ function CountDueNoteToday($class_id, $member_id){
 	$conn -> close();
 }
 
+// Insert into note table from the calendar InsertClassCalendarEvent() function
+function InsertNoteClassCalendar($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $event_id){
+	$conn = OpenCon();
+	// Convert the date to the proper format
+	$date = date('Y-m-d H:i:s', strtotime($start_datetime));
+	$sql = "INSERT INTO note (class_id, subject_id, post_date, due_date, due_time, note_title, description)
+			VALUES ('$class_id', ".
+			($subject_id == null ? "NULL" : "'$subject_id'")
+			.", CURDATE(), DATE('$date'), TIME('$date'), '$event_title', '$description')";
+	if($conn->query($sql) === TRUE){
+		// Insert into the note_calendar table so the calendar and note can be referenced
+		$sql = "INSERT INTO note_calendar (note_id, event_id)
+				VALUES ('".$conn->insert_id."', '$event_id')";
+		if ($conn->query($sql) === TRUE) {
+			echo "Note created! \n";
+		} else {
+	 	 	echo "Error: " . $sql . "<br>" . $conn->error;
+	 	}
 
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
+}
+
+// Update note table after updating the calendar table. From UpdateClassCalendarEvent() function
+function UpdateNoteClassCalendar($event_title, $description, $start_datetime, $end_datetime, $class_id, $subject_id, $event_id){
+	$conn = OpenCon();
+	// Convert the date to the proper format
+	$date = date('Y-m-d H:i:s', strtotime($start_datetime));
+	$sql = "UPDATE note
+			SET subject_id = ".($subject_id == null ? "NULL" : "'$subject_id'").", due_date = DATE('$date'), due_time = TIME('$date'), note_title = '$event_title', description = '$description'
+			WHERE note_id = (SELECT note_id
+				FROM note_calendar
+				WHERE event_id = '$event_id')";
+	if($conn->query($sql) === TRUE){
+		echo "Note updated! \n";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
+}
+
+// Delete note table after deleting from the calendar table
+function DeleteNoteClassCalendar($event_id){
+	$conn = OpenCon();
+	// Cleart the query
+	$conn->next_result();
+	$sql = "DELETE FROM note
+			WHERE note_id = (SELECT note_id
+				FROM note_calendar
+				WHERE event_id = '$event_id');
+			DELETE FROM note_calendar
+			WHERE event_id = '$event_id'";
+	if($conn->multi_query($sql) === TRUE){
+		echo "Note removed! \n";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
+}
 ?>
