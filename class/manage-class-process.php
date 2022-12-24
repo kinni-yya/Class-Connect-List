5cc169c2ea00b5cbd5a4d1f58f29c77890c808db
jimlozada05 <109643475+jimlozada05@users.noreply.github.com>
2022-12-23 17:20:10 -0800

    Add files via upload
    Fixed the subjects

--- class/manage-class-process.php
+++ class/manage-class-process.php
@@ -1,48 +1,94 @@
 <?php
 include '../dbconnect.php';
 OpenSession();
-// Open database connection for the nl2br
-$conn = OpenCon();
-
-$subject_details = NULL;
-// Check if subject_code is empty if not properly put the strings for SQL format
-if (isset($_POST['subject_details']) && !empty($_POST['subject_details'])) {
-	//nl2br converts the line breaks
-	//mysqli_real_escape_string converts the special characters
-	$subject_details = mysqli_real_escape_string($conn, nl2br($_POST['subject_details']));
-}
-
-$professor = NULL;
-if (isset($_POST['professor']) && !empty($_POST['professor'])) {
-	$professor = mysqli_real_escape_string($conn, nl2br($_POST['professor']));
-}
-
-// INSERT to subject table
-$subject_id = 0;
-if (isset($_POST['subject_name']) && !empty($_POST['subject_name'])) {
-	// Send the parameters to InsertSubject() function to insert to the subject table then return subject id
-	$subject_id = InsertSubject(
-		$_POST['subject_name'],
-		$subject_details,
-		$professor,
-		$_POST['class_id'],
-		$conn
-	);
-}
-
-// Insert the schedule for the subject using the returned subject id
-if($subject_id > 0){
-	for ($i=0; $i < sizeof($_POST['day']); $i++) { 
-		InsertSubjectSchedule($subject_id, $_POST['from_time'][$i], $_POST['to_time'][$i], $_POST['day'][$i], $_POST['class_id']);
-	}
-
-	header("Location: manage-class.php?class_id=".$_POST['class_id']);
-}
 
 // Delete the subject from the table
-if(isset($_POST['process_type']) || !empty($_POST['process_type'])){
+if($_POST['process_type'] == "delete"){
 	$delete_subject_message = DeleteSubject($_POST['subject_id']);
 	echo $delete_subject_message;
 }
 
+// Insert into subject table then to subject schedule table
+if($_POST['process_type'] == "insert"){
+	// Open database connection for the nl2br
+	$conn = OpenCon();
+
+	$subject_details = NULL;
+	// Check if subject_code is empty if not properly put the strings for SQL format
+	if (isset($_POST['subject_details']) && !empty($_POST['subject_details'])) {
+		//nl2br converts the line breaks
+		//mysqli_real_escape_string converts the special characters
+		$subject_details = mysqli_real_escape_string($conn, nl2br($_POST['subject_details']));
+	}
+
+	$professor = NULL;
+	if (isset($_POST['professor']) && !empty($_POST['professor'])) {
+		$professor = mysqli_real_escape_string($conn, nl2br($_POST['professor']));
+	}
+
+	// INSERT to subject table
+	$subject_id = 0;
+	if (isset($_POST['subject_name']) && !empty($_POST['subject_name'])) {
+		// Send the parameters to InsertSubject() function to insert to the subject table then return subject id
+		$subject_id = InsertSubject(
+			$_POST['subject_name'],
+			$subject_details,
+			$professor,
+			$_POST['class_id'],
+			$conn
+		);
+	}
+
+	// Insert the schedule for the subject using the returned subject id
+	if($subject_id > 0){
+		for ($i=0; $i < sizeof($_POST['start_date']); $i++) { 
+			InsertSubjectSchedule($subject_id, $_POST['from_time'][$i], $_POST['to_time'][$i], $_POST['start_date'][$i], $_POST['occurrence'][$i], $_POST['class_id']);
+		}
+	}
+}
+
+// Update subject table then subject schedule table
+if($_POST['process_type'] == "update"){
+	// Open database connection for the nl2br
+	$conn = OpenCon();
+
+	$subject_details = NULL;
+	// Check if subject_code is empty if not properly put the strings for SQL format
+	if (isset($_POST['subject_details']) && !empty($_POST['subject_details'])) {
+		//nl2br converts the line breaks
+		//mysqli_real_escape_string converts the special characters
+		$subject_details = mysqli_real_escape_string($conn, nl2br($_POST['subject_details']));
+	}
+
+	$professor = NULL;
+	if (isset($_POST['professor']) && !empty($_POST['professor'])) {
+		$professor = mysqli_real_escape_string($conn, nl2br($_POST['professor']));
+	}
+
+	if (isset($_POST['subject_id']) && !empty($_POST['subject_id'])) {
+
+		// Update subject table
+		if(UpdateSubject($_POST['subject_id'], $_POST['subject_name'], $subject_details, $professor, $_POST['class_id'], $conn) == TRUE){
+			// Update the subject schedule by loopin on the array
+			for ($i=0; $i < sizeof($_POST['start_date']); $i++) { 
+				// If the subject schedule is not set it means it is a new subject schedule. Insert the data to subject schedule
+				if(!isset($_POST['subject_schedule_id'][$i]) || empty($_POST['subject_schedule_id'][$i])){
+					InsertSubjectSchedule($_POST['subject_id'], $_POST['from_time'][$i], $_POST['to_time'][$i], $_POST['start_date'][$i], $_POST['occurrence'][$i], $_POST['class_id']);
+				}
+				// If the subject schedule is already set the row needs to be updated
+				else{
+					UpdateSubjectSchedule($_POST['subject_schedule_id'][$i], $_POST['subject_id'], $_POST['from_time'][$i], $_POST['to_time'][$i], $_POST['start_date'][$i], $_POST['occurrence'][$i], $_POST['class_id']);
+				}
+			}
+		}
+	}
+}
+
+// Delete subject schedule
+if ($_POST['process_type'] == "delete-schedule") {
+	// Delete subject schedule
+	if(DeleteSubjectSchedule($_POST['subject_schedule_id']) == TRUE){
+		echo "Schedule Deleted";
+	}
+}
 ?>
--- class/manage-class.php
+++ class/manage-class.php
@@ -32,59 +32,7 @@ $class_info = GetClassRecord($_GET['class_id']);
     </div>
 
     <!-- BLUR -->
-    <div id="blur" onclick="closeAddClassForm()"></div>
-
-    <!-- POPUP -->
-    <div id="popup">
-        <form action="manage-class-process.php" method="POST" class="form-container" id="addSubjectForm">
-            <p class="addsubj-title">
-                ADD SUBJECT for
-                <?php
-                $class_name = $class_info['class_name'];
-                echo "<span class='class_name'>$class_name</span>";
-                ?>
-            </p>
-            <div id="add-subj-form">
-                <input type="hidden" name="class_id" value="<?php echo $_GET['class_id']; ?>">
-
-                <label for="subject"><b>SUBJECT</b></label>
-                <input type="text" placeholder="e.g. English" name="subject_name" required><br><br>
-
-                <label for="subject"><b>Subject Details</b></label>
-                <input type="text" placeholder="e.g. EN 1001" name="subject_details"><br><br>
-
-                <label for="subject"><b>PROFESSOR</b></label>
-                <input type="text" placeholder="e.g. Juan Dela Cruz" name="professor"><br><br>
-            </div>
-            <label class="MT"><b>MEETING TIME</b></label>
-            <button onclick="addSubjectDate(event)">Add Meeting Time</button>
-
-            <div id="scheduled_time">
-                <label for="day"><b>DAY</b></label>
-                <select name="day[]">
-                    <option value="1">MON</option>
-                    <option value="2">TUE</option>
-                    <option value="3">WED</option>
-                    <option value="4">THU</option>
-                    <option value="5">FRI</option>
-                    <option value="6">SAT</option>
-                    <option value="7">SUN</option>
-                </select>
-
-                <label for="from"><b>FROM</b></label>
-                <input type="time" id="time1" name="from_time[]" required>
-
-                <label for="to"><b>TO</b></label>
-                <input type="time" id="time2" name="to_time[]" required>
-                <button onclick="removeSubjectDate(this)">Remove</button>
-                <br>
-            </div>
-            <div id="output"></div>
-            <div class="submit">
-                <button type="submit">Add Subject</button>
-            </div>
-        </form>
-    </div>
+    <div id="blur" onclick="closeClassForm()"></div>
 
     <br>
     <div class="classcase">
@@ -115,65 +63,269 @@ $class_info = GetClassRecord($_GET['class_id']);
                 <?php } ?>
                 </div>
                 <div class="subjbuttons">
-                    <button>Edit</button>
-                    <button onclick="RemoveSubject(this)">Delete</button>
+                    <button onclick="openEditClassForm(this)" data-subject-id="<?php echo $subj_row['subject_id']; ?>">Edit</button>
+                    <button onclick="RemoveSubject(this)" data-subject-id="<?php echo $subj_row['subject_id']; ?>">Delete</button>
                 </div>
             </div>
-        <?php } ?>
+
+        <?php 
+        $row_subject_info = SelectSubjectRecord($subj_row['subject_id']);?>
+        <!-- Edit subject POPUP -->
+        <div class="popup" id="subject-<?php echo $subj_row['subject_id']; ?>">
+            <form onsubmit="EditSubjectForm(this);event.preventDefault();">
+                <p class="addsubj-title">
+                    Edit SUBJECT
+                </p>
+                <div id="add-subj-form">
+                    <input type="hidden" name="class_id" value="<?php echo $_GET['class_id']; ?>">
+
+                    <label for="subject"><b>SUBJECT</b></label>
+                    <input type="text" placeholder="e.g. English" name="subject_name" value="<?php echo $row_subject_info['subject_name']; ?>" required><br><br>
+
+                    <label for="subject"><b>Subject Details</b></label>
+                    <input type="text" placeholder="e.g. EN 1001" name="subject_details" value="<?php echo $row_subject_info['subject_details']; ?>"><br><br>
+
+                    <label for="subject"><b>PROFESSOR</b></label>
+                    <input type="text" placeholder="e.g. Juan Dela Cruz" name="professor" value="<?php echo $row_subject_info['professor']; ?>"><br><br>
+                </div>
+                <label class="MT"><b>MEETING TIME</b></label>
+                <button type="button" onclick="addSubjectDate(this)" data-subject-id="<?php echo $subj_row['subject_id']; ?>">Add Meeting Time</button>
+
+                <?php $result_subject_schedule = SelectSubjectScheduleRecord($subj_row['subject_id']);
+                while ($sub_sched_row = $result_subject_schedule->fetch_assoc()) {
+                ?>
+                <div id="scheduled_time">
+                    <input type="hidden" name="subject_schedule_id[]" value="<?php echo $sub_sched_row['subject_schedule_id']; ?>">
+                    <input type="hidden" name="subject_id" value="<?php echo $sub_sched_row['subject_id']; ?>">
+
+                    <label for="start_date"><b>Start Date</b></label>
+                    <input type="date" id="start_date" name="start_date[]" value="<?php echo $sub_sched_row['start_date']; ?>" required>
+
+                    <label for="from"><b>FROM</b></label>
+                    <input type="time" id="time1" name="from_time[]" value="<?php echo $sub_sched_row['from_time']; ?>" required>
+
+                    <label for="to"><b>TO</b></label>
+                    <input type="time" id="time2" name="to_time[]" value="<?php echo $sub_sched_row['to_time']; ?>" required>
+                    <br>
+
+                    <label for="occurrence"><b>Weekly Occurrence</b></label>
+                    <input type="number" id="occurrence" name="occurrence[]" min="1" value="<?php echo $sub_sched_row['occurrence']; ?>" required>
+
+                    <button type="button" id="remove_btn" onclick="removeSubjectSchedule(this)" data-subject-schedule-id="<?php echo $sub_sched_row['subject_schedule_id']; ?>">Remove</button>
+                    <br>
+                </div>
+                <?php } // Close for while while ($result_subject_schedule->fetch_assoc()) { ?>
+
+                <!-- Prepend new meeting form generated here -->
+                <div id="output"></div>
+
+                <p>The day of week will come from the start date and the weekly occurence is how many times it will reoccurr</p>
+
+                <div class="submit">
+                    <button type="submit">Save Subject</button>
+                    <button type="button" onclick="closeEditClassForm(this)" data-subject-id="<?php echo $row_subject_info['subject_id']; ?>">Close</button>
+                </div>
+            </form>
+        </div>
+        <!-- Edit subject POPUP -->    
+        <?php } // Close for while ($subj_row = $subj->fetch_assoc()) { ?>
     </div>
+
+    <!-- Add subject POPUP -->
+    <div class="popup" id="subject-add">
+        <form id="AddSubjectForm">
+            <p class="addsubj-title">
+                ADD SUBJECT for
+                <?php
+                $class_name = $class_info['class_name'];
+                echo "<span class='class_name'>$class_name</span>";
+                ?>
+            </p>
+            <div id="add-subj-form">
+                <input type="hidden" name="class_id" value="<?php echo $_GET['class_id']; ?>">
+
+                <label for="subject"><b>SUBJECT</b></label>
+                <input type="text" placeholder="e.g. English" name="subject_name" required><br><br>
+
+                <label for="subject"><b>Subject Details</b></label>
+                <input type="text" placeholder="e.g. EN 1001" name="subject_details"><br><br>
+
+                <label for="subject"><b>PROFESSOR</b></label>
+                <input type="text" placeholder="e.g. Juan Dela Cruz" name="professor"><br><br>
+            </div>
+            <label class="MT"><b>MEETING TIME</b></label>
+            <button type="button" onclick="addSubjectDate(this)">Add Meeting Time</button>
+
+            <div id="scheduled_time">
+                <label for="start_date"><b>Start Date</b></label>
+                <input type="date" id="start_date" name="start_date[]" required>
+
+                <label for="from"><b>FROM</b></label>
+                <input type="time" id="time1" name="from_time[]" required>
+
+                <label for="to"><b>TO</b></label>
+                <input type="time" id="time2" name="to_time[]" required>
+                <br>
+
+                <label for="occurrence"><b>Weekly Occurrence</b></label>
+                <input type="number" id="occurrence" name="occurrence[]" min="1" required>
+
+                <button onclick="removeSubjectDate(this)">Remove</button>
+                <br>
+            </div>
+
+            <!-- Prepend new meeting form generated here -->
+            <div id="output"></div>
+
+            <p>The day of week will come from the start date and the weekly occurence is how many times it will reoccurr</p>
+
+            <div class="submit">
+                <button type="submit">Add Subject</button>
+            </div>
+        </form>
+    </div>
+    <!-- Add subject POPUP -->
 
     <!-- AJAX / jQuery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-    <!-- Script for cloning or duplicating the input for the subject schedule -->
-    <script type="text/javascript">
-        function addSubjectDate(e) {
-            $('#scheduled_time').clone().prependTo('#output');
+<!-- Script for cloning or duplicating the input for the subject schedule -->
+<script type="text/javascript">
+    function addSubjectDate(e) {
+        var subject_id = $(e).attr('data-subject-id');
+        // Check if the scheduled time form template exist
+        if($(e).parent().children('#scheduled_time').length > 0){
+            var add_form = $(e).parent().children('#scheduled_time').clone().appendTo($(e).parent().children('#output'));
+            add_form.find('#remove_btn').removeAttr('data-subject-schedule-id');
+            add_form.find('input[name="subject_schedule_id[]"], #start_date, #time1, #time2, #occurrence').removeAttr('value');
         }
-
-        function removeSubjectDate(e) {
-            $(e).parent().remove();
+        // If not create it
+        else{
+            $('<div id="scheduled_time"></div>').insertAfter(e);
+            $(e).parent().children('#scheduled_time').append('<input type="hidden" name="subject_schedule_id[]">');
+            $(e).parent().children('#scheduled_time').append('<input type="hidden" name="subject_id" value='+subject_id+'>');
+            $(e).parent().children('#scheduled_time').append('<label for="start_date"><b>Start Date</b></label> <input type="date" id="start_date" name="start_date[]" required>');
+            $(e).parent().children('#scheduled_time').append('<label for="from"><b>FROM</b></label> <input type="time" id="time1" name="from_time[]" required>');
+            $(e).parent().children('#scheduled_time').append('<label for="to"><b>TO</b></label> <input type="time" id="time2" name="to_time[]" required> <br>');
+            $(e).parent().children('#scheduled_time').append('<label for="occurrence"><b>Weekly Occurrence</b></label> <input type="number" id="occurrence" name="occurrence[]" min="1" required>');
+            $(e).parent().children('#scheduled_time').append('<button type="button" id="remove_btn" onclick="removeSubjectSchedule(this)">Remove</button> <br>');
         }
-    </script>
+    }
 
-    <!-- Delete the subject -->
-    <script type="text/javascript">
-        function RemoveSubject(e) {
-            var subject_id = $(e).attr("data-subject-id");
+    function removeSubjectDate(e) {
+        $(e).parent().remove();
+    }
+</script>
 
-            $.ajax({
-                type: 'POST',
-                url: 'manage-class-process.php',
-                data: {
-                    "subject_id": subject_id,
-                    "process_type": "delete"
-                },
-                success: function(data) {
-                    alert(data);
-                    location.reload();
-                }
-            });
+<!-- Popup JS -->
+<script type="text/javascript">
+    function openAddClassForm() {
+        $("#subject-add").show();
+        document.getElementById("blur").style.display = "block";
+        document.getElementById("blur").style.filter = "blur(10px)";
+        document.getElementById("scrll").style.overflow = "hidden";
+    }
+
+    function closeClassForm() {
+        $(".popup").hide();
+        document.getElementById('blur').style.filter = "blur(0)";
+        document.getElementById('blur').style.display = "none";
+        document.getElementById('scrll').style.overflow = "auto";
+    }
+
+    function openEditClassForm(e){
+        var subject_id = $(e).attr('data-subject-id');
+        $("#subject-"+subject_id).show();
+        document.getElementById("blur").style.display = "block";
+        document.getElementById("blur").style.filter = "blur(10px)";
+        document.getElementById("scrll").style.overflow = "hidden";
+    }
+
+    function closeEditClassForm(e){
+        var subject_id = $(e).attr('data-subject-id');
+        $("#subject-"+subject_id).hide();
+        document.getElementById('blur').style.filter = "blur(0)";
+        document.getElementById('blur').style.display = "none";
+        document.getElementById('scrll').style.overflow = "auto";
+    }
+</script>
+
+<!-- Add subject -->
+<script type="text/javascript">
+$("#AddSubjectForm").submit(function(e){
+    e.preventDefault();
+    var form = $(this);
+
+    $.ajax({
+        type: 'POST',
+        url: 'manage-class-process.php',
+        data: form.serialize() + "&process_type=insert",
+        success: function(data){
+            location.reload();
         }
-    </script>
+    });
+});
+</script>
 
+<!-- Delete the subject -->
+<script type="text/javascript">
+    function RemoveSubject(e) {
+        var subject_id = $(e).attr("data-subject-id");
 
-    <!-- Popup JS -->
-    <script type="text/javascript">
-        function openAddClassForm() {
-            document.getElementById("popup").style.display = "block";
-            document.getElementById("blur").style.display = "block";
-            document.getElementById("blur").style.filter = "blur(10px)";
-            document.getElementById("scrll").style.overflow = "hidden";
+        $.ajax({
+            type: 'POST',
+            url: 'manage-class-process.php',
+            data: {
+                "subject_id": subject_id,
+                "process_type": "delete"
+            },
+            success: function(data) {
+                alert(data);
+                location.reload();
+            }
+        });
+    }
+</script>
+
+<!-- Edit subject -->
+<script type="text/javascript">
+function EditSubjectForm(e){
+    var form = $(e);
+
+    $.ajax({
+        type: 'POST',
+        url: 'manage-class-process.php',
+        data: form.serialize() + "&process_type=update",
+        success: function(data){
+            location.reload();
         }
+    });
+}
+</script>
 
-        function closeAddClassForm() {
-            document.getElementById("popup").style.display = "none";
-            document.getElementById('blur').style.filter = "blur(0)";
-            document.getElementById('blur').style.display = "none";
-            document.getElementById('scrll').style.overflow = "auto";
-        }
-    </script>
+<!-- Remove the schedule from the subject schedule -->
+<script type="text/javascript">
+function removeSubjectSchedule(e){
+    var subject_schedule_id = $(e).attr("data-subject-schedule-id");
+    if(typeof $(e).attr("data-subject-schedule-id") !== 'undefined' && $(e).attr("data-subject-schedule-id") !== false){
+        $.ajax({
+            type: 'POST',
+            url: 'manage-class-process.php',
+            data: {
+                "subject_schedule_id": subject_schedule_id,
+                "process_type": "delete-schedule"
+            },
+            success: function(data){
+                alert(data);
+                location.reload();
+            }
+        });
+    }
+    else{
+        $(e).parent().remove();
+    }
+}
+</script>
 
 </body>
-
 </html>
